7c74300fb507f97cf401a7df8411c0ea72dbfe3d
Oscar702 <FunesLV@aol.com>
2020-12-15 01:45:27 +0000

    update

--- src/main.js
+++ src/main.js
@@ -6,5 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+  let pronoun = ['the','our','my'];
+  let adj = ['great', 'big','amazing' ];
+  let noun = ['jogger','racoon','tiger'];
+  let dtl = ['com','net']
+
+  for (let i = 0; i < pronoun.length; i++) {
+       for (let j = 0; j < adj.length; j++) {
+          for (let k = 0; k < noun.length; k++) {    
+              for (let l = 0; l < dtl.length; l++) {
+document.querySelector("#domain_name").innerHTML +=
+            '<li class="py-2 text-secondary"><h3> www.'   +
+                pronoun[i] +
+                adj[j] +
+                noun[k] +
+                dtl[l] +
+            (" </h3></li> ");
+              }
+            }
+         }
+      } 
+    };
